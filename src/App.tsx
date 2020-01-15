4e672848b75a7fa1e582af5d91c4664d0327a114
chucknerd <chucknerd1@gmail.com>
2020-01-15 04:25:35 -0600

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { stringify } from "querystring";
 
-const App: React.FC = () => {
+const App: React.FC = (): JSX.Element => {
+
+  interface Iobj {
+    a: number;
+    b: Array<string>;
+    c: string;
+  }
+
+  const [test, setTest] = useState<string>("");
+  const [num, setNum] = useState<number>(3);
+  const [myArr, setMyArr] = useState<Array<string>>([]);
+  const [obg, setByObj] = useState<Iobj>({a: 2, b:[], c: ""});
+
+  setMyArr(["3"]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +36,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
